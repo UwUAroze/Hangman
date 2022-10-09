47535af948ca18c6db68d2af4a6be49e0ddcae1f
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-10-09 17:01:22 +0100

    Added word list and getRandomWord()

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 public class Main {
 
+    static int totalGuesses = 0;
+    static int incorrectGuesses = 0;
+
+    // Word list stolen from: https://github.com/UwUAroze/ChatMinigames/blob/master/src/main/resources/config.yml
+    public static String[] words = {
+            "Attractive", "Baguette", "Bathtub", "Bedrock", "Broadcast", "Brother", "Ceiling", "Character", "Choke",
+            "Compartment", "Competitive", "Conclusion", "Confession", "Couple", "Cucumber", "Daddy", "Defenestration",
+            "Diamond", "Discombobulated", "Duvet", "Eggplant", "Emerald", "Extension", "Fishing", "Floor", "Generator",
+            "Greeting", "Hypothesis", "Immortal", "Improvement", "Invasion", "Iron", "Journal", "Knowledge", "Legend",
+            "Marriage", "Mommy", "Multiplier", "Nerd", "Obsidian", "Photosynthesis", "Pillow", "Prestige", "Raid",
+            "Sister", "Stick", "Suck", "Summer", "Swallow", "Transparent", "Treat", "Unique", "Villager", "Vomit",
+            "Winter"
+    };
+
     public static void main(String[] args) {
-        //
+        printGame();
     }
+
+    public static void printGame() {
+        System.out.println(HangingMan.getFunny(incorrectGuesses));
+        System.out.println(getRandomWord());
+    }
+
+    public static String getRandomWord() { return words[(int) (Math.random() * words.length)]; }
 
 }
