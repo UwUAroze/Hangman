c2d80a1b82aaae88dd7622b72acefe11868a849a
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-10-09 22:50:17 +0100

    done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,31 +14,40 @@ public class Main {
             "Winter"
     };
 
-    static ArrayList<String> correctGuesses = new ArrayList<>();
+    static ArrayList<String> guesses = new ArrayList<>();
     static ArrayList<String> incorrectGuesses = new ArrayList<>();
     static String word = getRandomWord();
 
     public static void main(String[] args) {
+        System.out.println("\nHangman, woah!");
         while (true) nextTurn();
     }
 
     public static void printGame() {
-        System.out.println(HangingMan.getFunny(incorrectGuesses.size()));
-        System.out.println(word);
+        printFunnyMan();
         printPlaceHolders();
+        StringBuilder misses = new StringBuilder().append("Misses:\t");
+        for (String miss : incorrectGuesses) misses.append(miss);
+        System.out.println(misses);
     }
 
     public static String getRandomWord() { return words[(int) (Math.random() * words.length)]; }
     public static String getInput() { return new Scanner(System.in).next().toLowerCase(); }
+    public static void printFunnyMan() { System.out.println(HangingMan.getFunny(incorrectGuesses.size())); }
+
+    static boolean checkForWin() {
+        for (int x=0; x < word.length(); x++) if (!guesses.contains(String.valueOf(word.charAt(x)).toLowerCase())) return false;
+        return true;
+    }
 
     static void printPlaceHolders() {
-        StringBuilder placeHolders = new StringBuilder();
+        StringBuilder placeHolders = new StringBuilder().append("Word:\t");
 
         for (int x=0; x < word.length(); x++) {
             placeHolders.append(" ");
             String correctChar = String.valueOf(word.charAt(x));
 
-            if (correctGuesses.contains(correctChar.toLowerCase())) {
+            if (guesses.contains(correctChar.toLowerCase())) {
                 placeHolders.append(correctChar);
                 continue;
             } placeHolders.append("_");
@@ -49,12 +57,39 @@ public class Main {
     }
 
     static void nextTurn() {
-        System.out.print("Guess: ");
-        String input = getInput();
-        if (word.toLowerCase().contains(input.toLowerCase())) correctGuesses.add(input);
-        else incorrectGuesses.add(input);
+
+        System.out.println();
 
         printGame();
+        System.out.print("\nGuess: ");
+        String input = getInput();
+
+        while (guesses.contains(input)) {
+            System.out.print("You already tried that! Have another guess: ");
+            input = getInput();
+        }
+
+        while (input.length() != 1) {
+            System.out.print("Invalid input. Please try again: ");
+            input = getInput();
+        }
+
+        guesses.add(input);
+        if (!word.toLowerCase().contains(input.toLowerCase())) incorrectGuesses.add(input);
+
+        if (checkForWin()) {
+            printFunnyMan();
+            System.out.println("You wonn!");
+            System.out.println("Now only half a man is hanged! woo.");
+            System.exit(0);
+        }
+
+        if (incorrectGuesses.size() == 6) {
+            printFunnyMan();
+            System.out.println("Game over. The man has been hanged ;c");
+            System.out.println("The word was: '" + word + "'");
+            System.exit(0);
+        }
 
     }
 
